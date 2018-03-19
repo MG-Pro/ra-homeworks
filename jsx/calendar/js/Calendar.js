75eacd3cf9d1034cb5f44b977e4fa84dd1315402
MG <mg84@bk.ru>
2018-03-20 00:02:04 +0300

    HW1 Task#3 'calendar' in work add days

--- jsx/calendar/js/Calendar.js
+++ jsx/calendar/js/Calendar.js
@@ -1,12 +1,50 @@
 const Calendar = props => {
   const {date} = props;
-  const daysName = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+  const dateGenitive = date.toLocaleDateString('ru-RU', {day: 'numeric', month: 'long'});
+
+  const d = new Date(2017, 4, 19);
+  console.log(d);
+
   const main = (
-    <div className='ui-datepicker'>
-      <div className='ui-datepicker-material-header'>
-        <div className='ui-datepicker-material-day'>{daysName[date.getDay()]}</div>
-        <div className='ui-datepicker-material-date'></div>
+    <div className = 'ui-datepicker'>
+      <div className = 'ui-datepicker-material-header'>
+        <div
+          className = 'ui-datepicker-material-day'>{date.toLocaleDateString('ru-RU', {weekday: 'long'}).toUpperCase()}</div>
+        <div className = 'ui-datepicker-material-date'>
+          <div className = 'ui-datepicker-material-day-num'>{date.getDate()}</div>
+          <div className = 'ui-datepicker-material-month'>{dateGenitive.substring(dateGenitive.indexOf(' '))}</div>
+          <div className = 'ui-datepicker-material-year'>{date.getFullYear()}</div>
+        </div>
       </div>
+      <div className = 'ui-datepicker-header'>
+        <div className = 'ui-datepicker-title'>
+          <span
+            className = 'ui-datepicker-month'>{date.toLocaleDateString('ru-RU', {month: 'long'}).toUpperCase() + ' '}</span>
+          <span className = 'ui-datepicker-year'>{date.getFullYear()}</span>
+        </div>
+      </div>
+      <table className = 'ui-datepicker-calendar'>
+        <colgroup>
+          <col/>
+          <col/>
+          <col/>
+          <col/>
+          <col/>
+          <col className = 'ui-datepicker-week-end'/>
+          <col className = 'ui-datepicker-week-end'/>
+        </colgroup>
+        <thead>
+        <tr>
+          {Array.from({length: 7}).map((val, i) => {
+            const date = new Date(2017, 4, 15 + i);
+            return (
+              <th scope = "col"
+                title = {date.toLocaleDateString('ru-RU', {weekday: 'long'})}>{date.toLocaleDateString('ru-RU', {weekday: 'short'})}</th>
+            );
+          })}
+        </tr>
+        </thead>
+      </table>
     </div>
   );
   return main;
