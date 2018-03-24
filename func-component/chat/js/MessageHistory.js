473578004227ccbc8cad8081e1deec49b782d81a
MG <mg84@bk.ru>
2018-03-24 18:06:43 +0300

    HW2 Task#2 'chat' cmpl

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const MessageHistory = props => {
+  if (props.list.length === 0) {
+    return null;
+  }
+
+  return (
+    <ul>
+      {props.list.map(item => {
+        return getItem(item);
+      })}
+    </ul>
+  );
+
+  function getItem(item) {
+    if (item.type === 'message') {
+      return <Message key = {item.id} from = {item.from} message = {item}/>
+    } else if(item.type === 'response') {
+      return <Response key = {item.id} from = {item.from} message = {item}/>
+    } else {
+      return <Typing key = {item.id} from = {item.from} message = {item}/>
+    }
+
+  }
+};
