61720fbc42535524101019423325ff6cbfe7ebe6
MG <mg84@bk.ru>
2018-03-30 00:32:58 +0300

    HW4 Task#2 'layouts' cmpl

--- state/layouts/js/App.js
+++ state/layouts/js/App.js
@@ -4,15 +4,35 @@ const VIEW_LIST = "view_list";
 const VIEW_MODULE = "view_module";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      viewType: VIEW_MODULE
+    };
+  }
+  switchHandler(e) {
+    console.log("сменился тип вывода");
+
+    let viewType;
+    if(e.target.textContent === VIEW_MODULE) {
+      viewType = VIEW_LIST;
+    } else {
+      viewType = VIEW_MODULE
+    }
+    this.setState({
+      viewType: viewType
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="toolbar">
           <IconSwitch
-            icon={VIEW_MODULE}
-            onSwitch={() => console.log("сменился тип вывода")} />
+            icon={this.state.viewType}
+            onSwitch={(e) => this.switchHandler(e)} />
         </div>
-        {this.renderLayout(true)}
+        {this.renderLayout(this.state.viewType === VIEW_MODULE)}
       </div>
     );
   }
