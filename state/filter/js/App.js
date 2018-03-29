fea33769009481ec79d0a9b696504be791918975
MG <mg84@bk.ru>
2018-03-29 22:34:28 +0300

    HW4 Task#3 'filter' cmpl

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,36 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filter: null,
+      projects: null
+    };
+  }
+
+  setFilter(filter)  {
+    const filtered = this.props.projects.filter(item => {
+      return item.category === filter;
+    });
+    this.setState({
+      filter: filter,
+      projects: filtered
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters = {this.props.filters}
+          selected = {this.state.filter || 'All'}
+          onSelectFilter = {filter => this.setFilter(filter)}/>
+        <Portfolio projects = {this.state.projects || this.props.projects}/>
+      </div>
+    );
+  }
+}
+
+
+
