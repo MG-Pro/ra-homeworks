941b71dc418ad0f0e459e73fd27dd7eca0756bb1
MG <mg84@bk.ru>
2018-04-01 12:57:40 +0300

    HW5 Task#1 'listing' cmpl 2 var

--- composition/listing/js/App.js
+++ composition/listing/js/App.js
@@ -1,16 +1,47 @@
 'use strict';
 
-const App = ({items}) => (
+// v.1
+
+//const App = (props) => (
+//    <List items = {props.items} />
+//);
+//
+//const List = props => {
+//   return (
+//     <main>
+//       {props.items.map(item => {
+//         switch(item.type) {
+//           case 'unisex':
+//             return <Item color="black" item={item} />;
+//           case 'male':
+//             return <Item color="blue" item={item} />;
+//           case 'female':
+//             return <Item color="orange" item={item} />;
+//         }
+//       })}
+//     </main>
+//   );
+//};
+
+// v. 2
+
+const App = (props) => (
   <main>
-    {items.map(item => {
-      switch(item.type) {
-        case 'unisex':
-          return <Item color="black" item={item} />;
-        case 'male':
-          return <Item color="blue" item={item} />;
-        case 'female':
-          return <Item color="orange" item={item} />;
-      }
-    })}
+    {getItems(props.items)}
   </main>
 );
+
+const getItems = items => {
+  return items.map(item => {
+        switch(item.type) {
+          case 'unisex':
+            return <Item color="black" item={item} />;
+          case 'male':
+            return <Item color="blue" item={item} />;
+          case 'female':
+            return <Item color="orange" item={item} />;
+        }
+      })
+
+
+};
