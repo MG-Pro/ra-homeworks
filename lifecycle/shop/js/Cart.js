d84fc3b51891facda4341ee6cadac6a9cebfaca0
MG <mg84@bk.ru>
2018-04-14 18:33:32 +0300

    HW9 Task#3 'shop' cmpl

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,8 +1,20 @@
 class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: props.isOpen
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      isOpen: newProps.items.length !== 0
+    })
+  }
 
   render() {
     return (
-      <CartView {...this.props} />
+      <CartView {...this.props} isOpen = {this.state.isOpen}/>
     );
   }
 
