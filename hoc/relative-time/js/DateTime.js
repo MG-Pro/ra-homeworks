7c14dff66539e2dd419049652236abd5c7c2ff2b
MG <mg84@bk.ru>
2018-04-16 23:52:16 +0300

    HW10 Task#1 'time' cmpl

--- hoc/relative-time/js/DateTime.js
+++ hoc/relative-time/js/DateTime.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const DateTime = props => {
-    return (
-        <p className="date">{props.date}</p>
-    )
+  return (
+    <p className = "date">{props.date}</p>
+  )
+};
+
+
+const DateTimePretty = props => {
+  function getPublicTime(date) {
+    const time = (Date.now() - (new Date(date)).getTime()) / 1000 / 60 / 60;
+    if(time < 1) {
+      return'12 минут назад';
+    } else if(time >= 1 && time < 24) {
+      return '5 часов назад';
+    } else {
+      return 'X дней назад'
+    }
+  }
+  return <DateTime date = {getPublicTime(props.date)}/>
 };
