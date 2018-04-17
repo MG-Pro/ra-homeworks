fd2db1827158093ca2d7cfd3d2cea83105c7b3de
MG <mg84@bk.ru>
2018-04-17 21:49:19 +0300

    HW10 Task#2 'highlighting-blocks' cmpl

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -1,17 +1,39 @@
 'use strict';
 
 const List = props => {
-    return props.list.map(item => {
-        switch (item.type) {
-            case 'video':
-                return (
-                    <Video {...item} />
-                );
+  return props.list.map(item => {
+    switch (item.type) {
+      case 'video':
+        return (
+          <ViewsWrap views = {item.views}>
+            <Video {...item} />
+          </ViewsWrap>
+        );
 
-            case 'article':
-                return (
-                    <Article {...item} />
-                );
-        }
-    });
+      case 'article':
+        return (
+          <ViewsWrap views = {item.views}>
+            <Article {...item} />
+          </ViewsWrap>
+        );
+    }
+  });
+};
+
+const ViewsWrap = props => {
+  if (props.views >= 1000) {
+    return (
+      <Popular>
+        {props.children}
+      </Popular>
+    );
+  } else if (props.views <= 100) {
+    return (
+      <New>
+        {props.children}
+      </New>
+    );
+  } else {
+    return props.children;
+  }
 };
