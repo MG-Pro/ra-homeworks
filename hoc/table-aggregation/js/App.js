b6e1e4c62f4c571dc735ce43e81e7b053184ee2f
MG <mg84@bk.ru>
2018-04-18 23:54:10 +0300

    HW10 Task#3 'tables' in work

--- hoc/table-aggregation/js/App.js
+++ hoc/table-aggregation/js/App.js
@@ -1,26 +1,50 @@
 'use strict';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    };
+  }
 
-    componentDidMount() {
-        axios.get('https://api.myjson.com/bins/l2s9l').then(response => {
-            this.setState(response.data);
-        });
-    }
+  componentDidMount() {
+    axios.get('https://api.myjson.com/bins/l2s9l').then(response => {
+      this.setState(response.data);
+    });
+  }
 
-    render() {
-        return (
-            <div id="app">
-                <MonthTable list={this.state.list} />
-                <YearTable list={this.state.list} />
-                <SortTable list={this.state.list} />
-            </div>
-        );
+  render() {
+    return (
+      <div id = "app">
+        <SortTableWrap>
+          <MonthTable list = {this.state.list}/>
+          <YearTable list = {this.state.list}/>
+          <SortTable list = {this.state.list}/>
+        </SortTableWrap>
+      </div>
+    );
+  }
+}
+
+class SortTableWrap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      children: this.props.children
     }
-};
+  }
+
+  componentDidUpdate() {
+    const children = this.props.children.map(item => {
+      console.log(item);
+       item.props.list
+    });
+  }
+
+
+  render() {
+    return this.props.children;
+  }
+
+}
