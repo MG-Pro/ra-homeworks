44ea13f01f3e7d7fc19e9bb71557c3471cb15f4a
MG <mg84@bk.ru>
2018-04-22 21:07:50 +0300

    HW10 Task#1 'time' fxd

--- hoc/relative-time/js/DateTime.js
+++ hoc/relative-time/js/DateTime.js
@@ -1,22 +1,8 @@
 'use strict';
 
 const DateTime = props => {
+  //debugger
   return (
     <p className = "date">{props.date}</p>
   )
-};
-
-
-const DateTimePretty = props => {
-  function getPublicTime(date) {
-    const time = (Date.now() - (new Date(date)).getTime()) / 1000 / 60 / 60;
-    if(time < 1) {
-      return'12 минут назад';
-    } else if(time >= 1 && time < 24) {
-      return '5 часов назад';
-    } else {
-      return 'X дней назад'
-    }
-  }
-  return <DateTime date = {getPublicTime(props.date)}/>
 };
--- hoc/relative-time/js/Video.js
+++ hoc/relative-time/js/Video.js
@@ -8,3 +8,21 @@ const Video = props => {
         </div>
     )
 };
+
+const DateTimePretty = dateTimeHoc(DateTime);
+
+function dateTimeHoc(Component) {
+  return (props, ...args) => {
+    function getPublicTime(date) {
+      const time = (Date.now() - (new Date(date)).getTime()) / 1000 / 60 / 60;
+      if(time < 1) {
+        return'12 минут назад';
+      } else if(time >= 1 && time < 24) {
+        return '5 часов назад';
+      } else {
+        return 'X дней назад'
+      }
+    }
+    return <Component date = {getPublicTime(props.date)} />;
+  };
+}
