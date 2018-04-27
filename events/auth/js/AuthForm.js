6185c1a0094a540d33f8723604f5b719395edaf3
MG <mg84@bk.ru>
2018-04-27 22:51:19 +0300

    HW3 Task#3 'auth' fxd

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,15 +1,14 @@
 'use strict';
 
 const AuthForm = props => {
-  console.log(props);
-
   let form;
+
   function submit(e) {
     e.preventDefault();
     const user = {};
-    user.name = form.querySelector('input[name=name]').value;
-    user.email = form.querySelector('input[name=email]').value;
-    user.pass = form.querySelector('input[name=pass]').value;
+    user.name = form.name.value;
+    user.email = form.email.value;
+    user.pass = form.pass.value;
     props.onAuth(user);
   }
 
@@ -17,12 +16,12 @@ const AuthForm = props => {
   function checkInput(e) {
     const regEmail = /[^\w@\.-]/i;
     const regPass = /\W/i;
-    if(e.target.type === 'email') {
-      if(regEmail.test(e.target.value)) {
+    if (e.target.type === 'email') {
+      if (regEmail.test(e.target.value)) {
         e.target.value = e.target.value.replace(regEmail, '');
       }
     } else {
-      if(regPass.test(e.target.value)) {
+      if (regPass.test(e.target.value)) {
         e.target.value = e.target.value.replace(regPass, '');
       }
     }
@@ -31,22 +30,22 @@ const AuthForm = props => {
   return (
     <form className = "ModalForm" action = "/404/auth/" method = "POST" ref = {elem => form = elem}>
       <div className = "Input">
-        <input required type = "text" placeholder = "Имя" name="name"/>
-          <label/>
+        <input required type = "text" placeholder = "Имя" name = "name"/>
+        <label/>
       </div>
       <div className = "Input">
-        <input type = "email" placeholder = "Электронная почта" onChange={checkInput} name="email"/>
-          <label/>
+        <input type = "email" placeholder = "Электронная почта" onChange = {checkInput} name = "email"/>
+        <label/>
       </div>
       <div className = "Input">
-        <input required type = "password" placeholder = "Пароль" onChange={checkInput} name="pass"/>
-          <label/>
+        <input required type = "password" placeholder = "Пароль" onChange = {checkInput} name = "pass"/>
+        <label/>
       </div>
-      <button type = "submit" onClick={submit}>
+      <button type = "submit" onClick = {submit}>
         <span>Войти</span>
         <i className = "fa fa-fw fa-chevron-right"/>
       </button>
     </form>
 
-);
+  );
 };
